1b7a1a1e8e88b5fc4d19fb85fc95e090632b878b
ariaProgrammerFlutter <87ariyaahady@gmail.com>
2024-02-02 21:31:28 +0330

    provider

--- lib/provider/shop_provider.dart
+++ lib/provider/shop_provider.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:wordpreesapp/api/api_service.dart';
+import 'package:wordpreesapp/model/woocammers/addto_cart_response.dart';
+import 'package:wordpreesapp/model/woocammers/addtocart_requast.dart';
 import 'package:wordpreesapp/model/wordpress/postmodel.dart';
 import 'package:wordpreesapp/model/woocammers/product_category_model.dart';
-
+import 'package:collection/collection.dart';
 import 'package:wordpreesapp/model/woocammers/productmodel.dart';
 
 class ShopProvider extends ChangeNotifier {
@@ -30,6 +32,10 @@ class ShopProvider extends ChangeNotifier {
   // store all posts
   List<PostsModel> _postModel = <PostsModel>[];
   List<PostsModel> get postsModel => _postModel;
+//
+  // item Shopping card
+  List<CardItem>? _itemsinCart;
+  List<CardItem>? get itemsinCart => _itemsinCart;
 //
 
   Future<void> getAllCategoryNames() async {
@@ -70,7 +76,38 @@ class ShopProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  Future<void> addToCart(CartProducts products, Function onCallBack) async {
+    AddToCartRequastModel requastModel = AddToCartRequastModel();
+    requastModel.products = <CartProducts>[];
+
+    if (_itemsinCart == null) initialaizeData();
+
+    _itemsinCart!.forEach((element) {
+      requastModel.products!.add(CartProducts(
+          productId: element.productId, quantity: element.quantity));
+    });
+    CartProducts? duplicatedProductId = requastModel.products!.firstWhereOrNull(
+      (prod) => prod.productId == products.productId,
+    );
+    if (duplicatedProductId != null) {
+      requastModel.products!.remove(duplicatedProductId);
+    }
+    requastModel.products!.add(products);
+
+    await _apiService.addToCart(requastModel).then((cartResponseModel) {
+      if (cartResponseModel) {
+        if (cartResponseModel.data != null) {
+          List<CartItem>? newCartResponseModel = cartResponseModel.data;
+          _itemsinCart!.addAll(newCartResponseModel!);
+        }
+      }
+      onCallBack(cartResponseModel);
+      notifyListeners();
+    });
+  }
+
   void initialaizeData() {
     _apiService = ApiService();
+    _itemsinCart = <CardItem>[];
   }
 }
